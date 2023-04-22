f8cca002451d6cf4996c2bde36d70fcac49226b2
Dan Stoian <danmihaistoian@gmail.com>
2023-04-22 16:45:17 +0300

    fixing bugs after testing

--- install.sh
+++ install.sh
@@ -1,27 +1,36 @@
 #!/bin/env zsh
 
-if [ "$#" -nq 2 ] then
+if [ "$#" -ne 2 ] then
   echo "Please provide your e-mail and Name"
-  echo "install.sh tim@apple.com 'Tim Apple' "
+  echo "install.sh tim@apple.com \"Tim Apple\" "
+  exit 1
 fi
 
 
 EMAIL=$1
-GIT_NAME=$2
+NAME=$2
+
+echo "Setting up environment with email $EMAIL and with name: $NAME"
 
 #update macOS
 softwareupdate -i
 
 #Install xcode tools
-xcode-select --install
+xcode-select --install &> /dev/null
+
+until $(xcode-select -p &> /dev/null); do
+    sleep 5;
+done
 
 # Copy .vimrc file
 # This file contains a custom setup to enable higlight in search, line numbering
 # and other features of vim 
+echo "Setting up vim..."
 cp .vimrc ~/.vimrc 
 
 
-# Install HomeBrew and Command Line Tools 
+# Install Homebrew and Command Line Tools 
+echo "Installing Homebrew"
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 
@@ -32,6 +41,8 @@ eval "$(/opt/homebrew/bin/brew shellevn)"
 
 
 # Install Oh-My-Zsh
+echo "Installing Oh-My-Zsh"
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Edit default theme to reflect username@machine_name in terminal prompt
@@ -39,10 +50,13 @@ sed 's/➜ :%/%n@%m ➜ :%/' ~/.oh-my-zsh/themes/robbyrussell.zsh-theme > !$
 
 
 ## configure git
-git config --global user.name "$GIT_NAME"
+echo "Configureing git..."
+git config --global user.name "$NAME"
 git config --global user.email "$EMAIL"
 
 #Create a new SSH-Key for git
+echo "Setting up SSH keys"
+mkdir -p ~/.ssh
 touch ~/.ssh/config
 
 echo << EOF > ~/.ssh/config
@@ -55,7 +69,7 @@ EOF
 
 
 # generate a new ssh-keygen
-ssh-keygen -t ed25519 -C "$EMAIL" -q -P ""
+ssh-keygen -t ed25519 -C "$EMAIL" -q -P "" -f id_ed25519
 
 # Start SSH agent
 eval "$(ssh-agent -s)"
@@ -68,7 +82,7 @@ pbcopy < ~/.ssh/id_ed25519.pub
 
 
 ## Apperance defaults ##
-
+echo "Configuring Desktop settings"
 # Dock size
 defaults write com.apple.dock magnification -bool true
 defaults write com.apple.dock largesize 65
@@ -93,6 +107,7 @@ defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
 
 # Install apps
+echo "Installing apps from homebrew..."
 brew install --cask discord battle-net element iina mactex dotnet-sdk steam transmission visual-studio-code
 
 
