0b6cec5817767340c6bed356afd20a2caa3c7e92
Stoian Dan <danmihaistoian@gmail.com>
2026-03-07 20:24:16 +0200

    Run Rust installation in the background

--- install.sh
+++ install.sh
@@ -60,7 +60,7 @@ wait $PID
 
 #Install Rust
 echo "Installing Rust... "
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y &
 
 PID=$!
 
